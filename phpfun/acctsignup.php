c5989a7b51c3d768cac2458164ef6ef0015ec7bd
ermarasigan <ermarasigan@outlook.com>
2017-06-28 15:06:29 +0800

    add prepared statements for login and signup

--- phpfun/acctsignup.php
+++ phpfun/acctsignup.php
@@ -62,32 +62,75 @@
 
     // Encrypt password
     $regpswd1 = sha1($regpswd1);
-      
-    $sql = "INSERT INTO users (
-      groupcode, username, password, role)
-      VALUES
-      ('$groupcode','$reguser','$regpswd1','regular')";
-        
-    if(mysqli_query($conn,$sql)){
-      $output .= "Account successfully created ";
-      
-      $signup_status = "signup_success";
-      return $output;
+
+
+    // Prepared statements
+    $stmt=mysqli_stmt_init($conn);
+    $role='regular';
+
+    $sql = "INSERT INTO users 
+                    (groupcode, username, password, role)
+            VALUES (?,?,?,?)";
+
+    if(mysqli_stmt_prepare($stmt,$sql)){
+      mysqli_stmt_bind_param($stmt,'ssss',$groupcode,$reguser,$regpswd1,$role);
+      mysqli_stmt_execute($stmt);
+
+      if(mysqli_stmt_affected_rows($stmt)>0) {
+        $output .= "Account successfully created ";
+        $signup_status = "signup_success";
+        return $output;
+      }
+
+      mysqli_stmt_close($stmt);
     }
+         
+    // $sql = "INSERT INTO users (
+    //   groupcode, username, password, role)
+    //   VALUES
+    //   ('$groupcode','$reguser','$regpswd1','regular')";
+        
+    // if(mysqli_query($conn,$sql)){
+    //   $output .= "Account successfully created ";
+      
+    //   $signup_status = "signup_success";
+    //   return $output;
+    // }
   }
 
   function exists($groupcode,$username){
 
     global $conn;
 
+    // Prepared statements
+    $stmt=mysqli_stmt_init($conn);
+
     $sql = "SELECT * FROM users 
-      WHERE username = '$username'
-        AND groupcode = '$groupcode'
+      WHERE username = ?
+        AND groupcode = ?
         ";
 
-    $result = mysqli_query($conn,$sql);
-    if(mysqli_num_rows($result) > 0) {
-      return true;
+    if(mysqli_stmt_prepare($stmt,$sql)){
+      mysqli_stmt_bind_param($stmt,'ss',$username,$groupcode);
+      mysqli_stmt_execute($stmt);
+
+      mysqli_stmt_store_result($stmt);
+      
+      if(mysqli_stmt_num_rows($stmt) > 0) {
+        return true;
+      }
+
+      mysqli_stmt_close($stmt);
     }
+
+    // $sql = "SELECT * FROM users 
+    //   WHERE username = '$username'
+    //     AND groupcode = '$groupcode'
+    //     ";
+
+    // $result = mysqli_query($conn,$sql);
+    // if(mysqli_num_rows($result) > 0) {
+    //   return true;
+    // }
   }
 ?>
