8a92ce5138e5be1a4930d48476621f904b21b07a
ermarasigan <ermarasigan@outlook.com>
2017-06-28 16:47:24 +0800

    allow admin to add cart

--- phpfun/showmenu.php
+++ phpfun/showmenu.php
@@ -11,59 +11,47 @@
 			if(mysqli_num_rows($result) > 0) {
 				while($row = mysqli_fetch_assoc($result)) {
 					extract($row);
-			    	// echo "rows fetched";
-
-			    	// isset($_GET['id']) ? $_GET['id'] : "";
-
-
+			    	
 					// Edit and delete buttons
-					if(!isset($_SESSION['role'])) $_SESSION['role'] = '';
-
+					if(!isset($_SESSION['role'])) { 
+						$_SESSION['role'] = '';
+					}
+			
 					if($_SESSION['role']=='admin'){
 				    	$buttons = 
 				    		"<div class='form-group'>
 				    			<a href='menu_update.php?id=$id'>
-									<button class='btn btn-default btn-lg'>
+									<button class='btn btn-warning btn-md'>
 										Edit 
 									</button>
 							  	</a>
 							  	<a href='menu_delete.php?id=$id'>
-									<button class='btn btn-default btn-lg'>
+									<button class='btn btn-warning btn-md'>
 										Delete 
 									</button>
 							  	</a>
 							</div>";
 					} else {
+						$buttons='';
+					}
 
-						if($_SESSION['checkout']=='yes'){
-							$buttons='';
-						} else {
-							$buttons = 
-							// "<div class='form-group'>
-				   			//  	<a href='menu_cart.php?id=$id'>
-							// 		<button class='btn btn-default btn-lg'>
-							// 			Add to Cart
-							// 		</button>
-							//   	</a>
-							// </div>";
-							
-							// min=0 step=1 style='background: transparent; width: 40px; font-size: 20px; ' placeholder='0'
-
-						  	"
-						  	 <input class='btn btn-default btn-md' type='submit' id='$id' name='addtocart' style:'padding-top: 20px; display:block;'
+					if($_SESSION['checkout']=='yes'){
+						$cartbtn='';
+						$buttons='';
+					} else {
+						$cartbtn = 
+						  	 "<input class='btn btn-default btn-lg' type='submit' id='$id' name='addtocart' style:'padding-top: 20px; display:block;'
 						  	 	value='Add to Cart' onclick='ajaxPost(this.id);'>
 					  	 	<input type='hidden' id='quantity$id' name='quantity' value=1>
 					  	 	<input type='hidden' id='itemname$id' name='itemname' value='$name'>
 					  	 	<input type='hidden' id='itemprice$id' name='itemprice' value=$price>
 					  	 	";
-					  	}
-					}
-
+					 }			
 
 					// Buttons to confirm or cancel Delete
 					$delete_confirmation = 
 						"<form method='post' action=''>
-							<h4>Are you sure you want to delete?</h4>
+							<h5>Are you sure you want to delete?</h5>
 							<button class='btn btn-default btn-sm' name='yes'>Yes</button>
 							<button class='btn btn-default btn-sm' name='no'>No</button>
 						</form>";
@@ -71,14 +59,14 @@
 
 					// Buttons to confirm or cancel Update
 					$update_confirmation = 
-						"<form method='post' action='' style='background-color: transparent; border: none; color: black; font-size: 0.5em'>
+						"<form method='post' action='' style='background-color: transparent; border: none; color: black; font-size: 0.54em'>
 		    				<div class='form-group'>
 								<input type='text' name='updname' placeholder=$name>
 							</div>
 							<div class='form-group'>
 								<input type='number' min=0 name='updprice' placeholder=$price>
 							</div>	
-							<h4 style='color:white'>Are you sure you want to update?</h4>
+							<h5 style='color:white'>Are you sure you want to update?</h5>
 							<br>
 							<button class='btn btn-default btn-sm' name='updyes'>Yes
 							</button>
@@ -91,6 +79,7 @@
 						    	$name <br>
 			    				$price <br>
 				    			$buttons
+				    			$cartbtn
 							  </div>";
 					}
 
